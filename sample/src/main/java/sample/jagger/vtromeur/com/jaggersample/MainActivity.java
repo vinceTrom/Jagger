499923842f8db6f118e1b294814b86689088a104
Vince <vincent.tromeur@gmail.com>
2016-03-22 12:27:21 +0100

    sample app added, jagger module declared as a library

---
+++ sample/src/main/java/sample/jagger/vtromeur/com/jaggersample/MainActivity.java
@@ -0,0 +1,18 @@
+package sample.jagger.vtromeur.com.jaggersample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vtromeur.jagger.ChatFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getFragmentManager().beginTransaction().add(R.id.fragment_container, ChatFragment.getInstance("vtrom", "azertyui", "testtest")).commit();
+
+    }
+
+}
